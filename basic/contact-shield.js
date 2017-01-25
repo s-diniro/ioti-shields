6d64b7581b90870ee7779f04ab5915a5bf0359bd
Patrick Pircher <patrick.pircher@ibm.com>
2017-01-25 13:57:21 +0100

    encapsulate each shield since the code is joined in the shield engine

--- basic/contact-shield.js
+++ basic/contact-shield.js
@@ -1,35 +1,37 @@
+(function () {
 
-var Shield = {
-  id: 5 /*must be a number*/,
-  name: 'contact-shield',
-  delay: 5000,
-  safelet: function (payload) {
-    /*value == 'closed/open'*/
-    return payload.d.states.contact.value === 'open';
-  },
+  var Shield = {
+    id: 5 /*must be a number*/,
+    name: 'contact-shield',
+    delay: 5000,
+    safelet: function (payload) {
+      /*value == 'closed/open'*/
+      return payload.d.states.contact.value === 'open';
+    },
 
-  entryCondition: function (payload) {
-    return payload.d && payload.d.states && payload.d.states.contact !== undefined;
-  },
+    entryCondition: function (payload) {
+      return payload.d && payload.d.states && payload.d.states.contact !== undefined;
+    },
 
-  preProcessing: undefined,
+    preProcessing: undefined,
 
-  message: function (payload) {
-    return constructMessage(
-      payload,
-      Shield.id,
-      'ContactHazard',
-      'Something is open!'
-    );
-  }
-};
+    message: function (payload) {
+      return constructMessage(
+        payload,
+        Shield.id,
+        'ContactHazard',
+        'Something is open!'
+      );
+    }
+  };
 
-registerShield(
-  Shield.id,
-  Shield.name,
-  Shield.entryCondition,
-  Shield.preProcessing,
-  Shield.safelet,
-  Shield.message,
-  Shield.delay
-);
+  registerShield(
+    Shield.id,
+    Shield.name,
+    Shield.entryCondition,
+    Shield.preProcessing,
+    Shield.safelet,
+    Shield.message,
+    Shield.delay
+  );
+})();
--- basic/temperature-shield.js
+++ basic/temperature-shield.js
@@ -1,35 +1,37 @@
+(function () {
 
-var Shield = {
-  id: 6 /*must be a number*/,
-  name: 'temperature-shield',
-  delay: 5000,
-  safelet: function (payload) {
-    /*value == <number>*/
-    return payload.d.states.temperature.value > 23;
-  },
+  var Shield = {
+    id: 6 /*must be a number*/,
+    name: 'temperature-shield',
+    delay: 5000,
+    safelet: function (payload) {
+      /*value == <number>*/
+      return payload.d.states.temperature.value > 23;
+    },
 
-  entryCondition: function (payload) {
-    return payload.d && payload.d.states && payload.d.states.temperature !== undefined;
-  },
+    entryCondition: function (payload) {
+      return payload.d && payload.d.states && payload.d.states.temperature !== undefined;
+    },
 
-  preProcessing: undefined,
+    preProcessing: undefined,
 
-  message: function (payload) {
-    return constructMessage(
-      payload,
-      Shield.id,
-      'TemperatureHazard',
-      'Its too hot!'
-    );
-  }
-};
+    message: function (payload) {
+      return constructMessage(
+        payload,
+        Shield.id,
+        'TemperatureHazard',
+        'Its too hot!'
+      );
+    }
+  };
 
-registerShield(
-  Shield.id,
-  Shield.name,
-  Shield.entryCondition,
-  Shield.preProcessing,
-  Shield.safelet,
-  Shield.message,
-  Shield.delay
-);
+  registerShield(
+    Shield.id,
+    Shield.name,
+    Shield.entryCondition,
+    Shield.preProcessing,
+    Shield.safelet,
+    Shield.message,
+    Shield.delay
+  );
+})();
