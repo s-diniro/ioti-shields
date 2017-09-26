2ee2af2a96117c867cf330f6b36cb29b489604f3
Bilal Al-Saeedi <bilal.alsaeedi@de.ibm.com>
2017-09-26 11:21:26 +0200

    Add the queryable simple shield

---
+++ iot4i-v2/examples/simple-shields/QuerableSimpleShield/impl/java/src/com/ibm/iot4i/examples/ExecuteJsonQueryImpl.java
@@ -0,0 +1,47 @@
+package com.ibm.iot4i.examples;
+
+import java.util.List;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.ibm.streams.function.model.Function;
+import com.ibm.streams.toolkit.model.ToolkitLibraries;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Option;
+
+@ToolkitLibraries("impl/lib/*")
+public class ExecuteJsonQueryImpl {
+
+	static Configuration conf = Configuration.defaultConfiguration().addOptions(Option.AS_PATH_LIST)
+			.addOptions(Option.ALWAYS_RETURN_LIST).addOptions(Option.SUPPRESS_EXCEPTIONS);
+
+	static Logger logger = Logger.getLogger(ExecuteJsonQueryImpl.class);
+
+	@Function(namespace = "com.ibm.iot4i.examples", name = "executeJsonQuery", description = "", stateful = false)
+	public static boolean executeJsonQuery(String message, String jsonQuery, String operatorName) {
+		try {
+			JsonObject messageJson = new JsonParser().parse(message).getAsJsonObject();
+			String rawEvent = messageJson.getAsJsonObject("event").toString();
+			logger.log(Level.WARN, operatorName + ": Json query: " + jsonQuery);
+			logger.log(Level.WARN, operatorName + ": Incoming event: " + rawEvent);
+
+			List<Object> results = JsonPath.using(conf).parse(rawEvent).read(jsonQuery);
+			if (results.size() != 0) {
+				logger.log(Level.WARN, operatorName + ": Event passed json quey, results: " + results);
+				return true;
+			}
+
+			logger.log(Level.WARN, operatorName + ": Event failed json query");
+			return false;
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.log(Level.WARN, operatorName + ": Executing json query failed, error: " + e.getMessage());
+			return false;
+		}
+	}
+
+}
